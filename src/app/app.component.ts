cb81f18ce7a0d537986bcdb00b168be79691ccab
MikesSzinyei <szinyeimikes@gmail.com>
2023-06-08 10:18:11 +0200

    Kész

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,28 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'hello';
+  title = 'Alma és körte';
+  name= "Nagy Ádám";
+
+  employee = {
+    name: 'Erős István',
+    city: 'Szeged',
+    salary:'384'
+  }
+
+  employees = [
+    {name: 'Brancs Ádám', city: 'Szolnok', salary:397},
+    {name: 'Fank Ferenc', city: 'Miskolc', salary:383},
+    {name: 'Prima Erik', city: 'Bidapest', salary:391},
+    {name: 'Lent Árpád', city: 'Szolnok', salary:367},
+    {name: 'Hisz Laura', city: 'Szeged', salary:374}
+  ]
+
+  constructor() {
+    this.kiir('valami')
+  }
+  kiir(msg:string):void {
+    console.log(msg)
+    console.log(this.employee.name)
+  }
 }
